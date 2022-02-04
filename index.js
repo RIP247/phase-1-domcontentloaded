1722d5565ed3163d46d25bd37079b6afb21a655b
Rodney <rodneypa@LAPTOP-KQJ5G51E.localdomain>
2022-02-04 13:51:44 -0500

    commit tree 73aa89e232a735f14c8599ab6cd6f5497d7a44a1

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    const main = document.getElementById('text');
+    return main.textContent = "This is really cool!";
+  });
+
+   console.log(document.getElementById("test").value);
+ 
+  
